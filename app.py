412fdc6dfbf1fd1bef009a2502ae1da870ad26b1
flagflag96-byte <flagflag96@gmail.com>
2026-07-10 08:58:58 -0700

    app.py

--- app.py
+++ app.py
@@ -11,39 +11,40 @@ st.title("⚙️ Admin Control Panel (Google Drive Secure Storage)")
 # የደህንነት ፈቃድ መለያዎች
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-# 🛠️ ማስተካከያ፦ ቁልፉን በቀጥታ እዚህ በማስቀመጥ የ Secrets ፎርማት ስህተትን ሙሉ በሙሉ መፍታት
-FIXED_KEY = (
-    "-----BEGIN PRIVATE KEY-----\n"
-    "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCn9OV6MlpqRsIE\n"
-    "XCGT+ElDm1KFTF5q3/7dgRuk63jgdMH1m4WFnDMfEdvh8EwX3cPbN9WQYllK9ZBe\n"
-    "Luit5UHK1vCPvzZw5r/l557QXic0aG1NKrwhP13qUm3klNkXwaptdacMaeckBMfE\n"
-    "6SXq+cNtzYe5AixhgRX3JUdLndiCwNBkEn1PULSfVmRYIKQ3zswP5RJEMTb27uW/\n"
-    "+PPd4Ltd0kyJEoWiKWYsf3MMR8usMxoKkpb/8J6ae1plc/RnnBmlbzxceNQ6f1Dj\n"
-    "x/L0X10ArZ2ZHdUNynma5xQ0ta6n2kmK//scm2Pyy2ugfp5tO8xFI1TYTTgtg5HL\n"
-    "CUHRmwrdAgMBAAECggEAN63J/U6ABsdaPlPEleczcoQlQSdmDYwgU5IfdfbR5acP\n"
-    "PbrG8torYFttR2N/9lGAQkYqOGlCLMVnjLGEVVGxE+tZaG39QERezXqRfYYY/Ri5\n"
-    "U2GdaHA3VBzrbWGi7ms3cn4UWNsq0xf6md6owVL3ZIlEOyLrT4+Zlor3LawwQHfd\n"
-    "YJQNH8+6sW/UAhd+QkJuMTDSkEhkrzxCE3fQTT6M20CRTHO/6EhOjRubp64pEhGt\n"
-    "nokemiuORrfI/+a1jDSyNvc4czUGbqgQnR9bY2UforZ2A/xcnsLMOL2mg/uMPfKuR\n"
-    "fXrpyaBWtE916RN2/J2vLqz25Pf01vH07+LRmwvaCQKBgQDcZm5RfdxWMpeEAn1e\n"
-    "OalYtzj1F7HGrcFOPbB61kmGYA6yiyVH8IEva4mNVSW0jyeS9DkqqfDDqbvEHXmc\n"
-    "I0LqnF5FH5vVoCZuvnfXMQqOGptKC9aUHk9xlDjBMuI9hm46bh3SqKDdzdGppMUg\n"
-    "eY1u4AV2yLzTf3GS5C8xCzfJgwKBgQDDFevW7ylaARC6dutPrm/qH+myB1idFvKE\n"
-    "9yxERqTTvFnadQSjVbL5TMbxxB7ORHxv4AOV1GhLSU+l+iVqT+aF8YZ7whQO2C/o\n"
-    "9Nv3R+33M9yQiMaAit4X53tAXDOoj78s3VgXksEu9x0gyK/+ey3WQw/Nrtyb1oAl\n"
-    "SwLdWuKMHwKBgC+AkDRWKSAW5VsZYkXlrZ8mVktU4QcOOgSP/TJsbwpcN665mL5\n"
-    "kp5S0iG5AVhqJkBhDNUqLW8ubP62z5gcbXa9XwAM8PxY+Q+TU6b4teqsx11tMcFY\n"
-    "ywySwqy5N9PE3osPYrQdjhZgHtkuXyJBsH7+H49bcn8vD552OcmbPlnbAoGBAI3h\n"
-    "MH9Fuq5Bnhfuit6Nf8knYn/ehdzhpol7dg9FzXRMRJDzj+b2Wc6jvwBv4POkrVdz\n"
-    "9UePjjZK4AlXLsTtzTrW2DbDhHDzW4yyIBPslS224w+e2bb8x99cs6+wHsXVnXUU\n"
-    "mY+zZJoykFWafxy2GzT72s98/+a6jsIQlsnD4oWDAoGBAKRVuFayf8dXk3RC1ra7\n"
-    "1unrjSdW37spBjNblEdZjwxdFRhRSZy+jZFNSqBInwrCS3ovp25XfppaQuh3e3Dw\n"
-    "QOJu2hcQ9q3x2OC1gkHdb+CxJPb2j9brRdL3ErMcf3r3kqjY5apgmv09XP5hTIl2\n"
-    "HovD/0ALakHDl5g5d8dQT25h\n"
-    "-----END PRIVATE KEY-----\n"
+# 🛠️ ማስተካከያ፦ ቁልፉን ያለ ምንም ክፍተት በአንድ መስመር በማዘጋጀት የፎርማት ስህተትን ሙሉ በሙሉ መፍታት
+RAW_KEY = (
+    "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCn9OV6MlpqRsIE"
+    "XCGT+ElDm1KFTF5q3/7dgRuk63jgdMH1m4WFnDMfEdvh8EwX3cPbN9WQYllK9ZBe"
+    "Luit5UHK1vCPvzZw5r/l557QXic0aG1NKrwhP13qUm3klNkXwaptdacMaeckBMfE"
+    "6SXq+cNtzYe5AixhgRX3JUdLndiCwNBkEn1PULSfVmRYIKQ3zswP5RJEMTb27uW/"
+    "+PPd4Ltd0kyJEoWiKWYsf3MMR8usMxoKkpb/8J6ae1plc/RnnBmlbzxceNQ6f1Dj"
+    "x/L0X10ArZ2ZHdUNynma5xQ0ta6n2kmK//scm2Pyy2ugfp5tO8xFI1TYTTgtg5HL"
+    "CUHRmwrdAgMBAAECggEAN63J/U6ABsdaPlPEleczcoQlQSdmDYwgU5IfdfbR5acP"
+    "PbrG8torYFttR2N/9lGAQkYqOGlCLMVnjLGEVVGxE+tZaG39QERezXqRfYYY/Ri5"
+    "U2GdaHA3VBzrbWGi7ms3cn4UWNsq0xf6md6owVL3ZIlEOyLrT4+Zlor3LawwQHfd"
+    "YJQNH8+6sW/UAhd+QkJuMTDSkEhkrzxCE3fQTT6M20CRTHO/6EhOjRubp64pEhGt"
+    "nokemiuORrfI/+a1jDSyNvc4czUGbqgQnR9bY2UforZ2A/xcnsLMOL2mg/uMPfKuR"
+    "fXrpyaBWtE916RN2/J2vLqz25Pf01vH07+LRmwvaCQKBgQDcZm5RfdxWMpeEAn1e"
+    "OalYtzj1F7HGrcFOPbB61kmGYA6yiyVH8IEva4mNVSW0jyeS9DkqqfDDqbvEHXmc"
+    "I0LqnF5FH5vVoCZuvnfXMQqOGptKC9aUHk9xlDjBMuI9hm46bh3SqKDdzdGppMUg"
+    "eY1u4AV2yLzTf3GS5C8xCzfJgwKBgQDDFevW7ylaARC6dutPrm/qH+myB1idFvKE"
+    "9yxERqTTvFnadQSjVbL5TMbxxB7ORHxv4AOV1GhLSU+l+iVqT+aF8YZ7whQO2C/o"
+    "9Nv3R+33M9yQiMaAit4X53tAXDOoj78s3VgXksEu9x0gyK/+ey3WQw/Nrtyb1oAl"
+    "SwLdWuKMHwKBgC+AkDRWKSAW5VsZYkXlrZ8mVktU4QcOOgSP/TJsbwpcN665mL5"
+    "kp5S0iG5AVhqJkBhDNUqLW8ubP62z5gcbXa9XwAM8PxY+Q+TU6b4teqsx11tMcFY"
+    "ywySwqy5N9PE3osPYrQdjhZgHtkuXyJBsH7+H49bcn8vD552OcmbPlnbAoGBAI3h"
+    "MH9Fuq5Bnhfuit6Nf8knYn/ehdzhpol7dg9FzXRMRJDzj+b2Wc6jvwBv4POkrVdz"
+    "9UePjjZK4AlXLsTtzTrW2DbDhHDzW4yyIBPslS224w+e2bb8x99cs6+wHsXVnXUU"
+    "mY+zZJoykFWafxy2GzT72s98/+a6jsIQlsnD4oWDAoGBAKRVuFayf8dXk3RC1ra7"
+    "1unrjSdW37spBjNblEdZjwxdFRhRSZy+jZFNSqBInwrCS3ovp25XfppaQuh3e3Dw"
+    "QOJu2hcQ9q3x2OC1gkHdb+CxJPb2j9brRdL3ErMcf3r3kqjY5apgmv09XP5hTIl2"
+    "HovD/0ALakHDl5g5d8dQT25h"
 )
 
-# የጉግል አካውንት መረጃዎችን እዚህ መሰብሰብ
+# ቁልፉን የፓይተን ላይብረሪዎች በሚፈልጉት ትክክለኛ የረድፍ መዋቅር ማደራጀት
+FIXED_KEY = f"-----BEGIN PRIVATE KEY-----\n{RAW_KEY}\n-----END PRIVATE KEY-----\n"
+
+# የጉግል ሰርቪስ አካውንት መረጃዎች
 creds_dict = {
     "type": "service_account",
     "project_id": "modified-badge-500709-q9",
